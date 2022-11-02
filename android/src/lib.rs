15018ca115c98d786de77298296797b5e3865c14
Takeru Ohta <phjgt308@gmail.com>
2022-11-02 22:19:41 +0900

    [android] [wip] Add file open dialog

--- android/src/lib.rs
+++ android/src/lib.rs
@@ -37,10 +37,10 @@ fn initialize() {
     let activity = ndk_glue::native_activity();
     let vm = unsafe { jni::JavaVM::from_raw(activity.vm()) }.unwrap_or_else(|e| panic!("{e}"));
     let env = vm.attach_current_thread().unwrap_or_else(|e| panic!("{e}"));
-
+    let activity_instance = unsafe { JObject::from_raw(activity.activity()) };
     let window = env
         .call_method(
-            unsafe { JObject::from_raw(activity.activity()) },
+            activity_instance,
             "getWindow",
             "()Landroid/view/Window;",
             &[],
@@ -73,4 +73,60 @@ fn initialize() {
 
     env.call_method(decor_view, "setSystemUiVisibility", "(I)V", &[flag])
         .unwrap_or_else(|e| panic!("{e}"));
+}
+
+fn start_file_open() {
+    let activity = ndk_glue::native_activity();
+    let vm = unsafe { jni::JavaVM::from_raw(activity.vm()) }.unwrap_or_else(|e| panic!("{e}"));
+    let env = vm.attach_current_thread().unwrap_or_else(|e| panic!("{e}"));
+
+    let activity = ndk_glue::native_activity();
+    let activity_instance = unsafe { JObject::from_raw(activity.activity()) };
+
+    let intent_class = env
+        .find_class("android/content/Intent")
+        .unwrap_or_else(|e| panic!("{e}"));
+    let action_create_document = env
+        .get_static_field(intent_class, "ACTION_GET_CONTENT", "Ljava/lang/String;")
+        //.get_static_field(intent_class, "ACTION_OPEN_DOCUMENT", "Ljava/lang/String;")
+        //.get_static_field(intent_class, "ACTION_CREATE_DOCUMENT", "Ljava/lang/String;")
+        .unwrap_or_else(|e| panic!("{e}"));
+    let category_openable = env
+        .get_static_field(intent_class, "CATEGORY_OPENABLE", "Ljava/lang/String;")
+        .unwrap_or_else(|e| panic!("{e}"));
+
+    let intent = env
+        .new_object(
+            intent_class,
+            "(Ljava/lang/String;)V",
+            &[action_create_document],
+        )
+        .unwrap_or_else(|e| panic!("{e}"));
+    env.call_method(
+        intent,
+        "addCategory",
+        "(Ljava/lang/String;)Landroid/content/Intent;",
+        &[category_openable],
+    )
+    .unwrap_or_else(|e| panic!("{e}"));
+    env.call_method(
+        intent,
+        "setType",
+        "(Ljava/lang/String;)Landroid/content/Intent;",
+        &[env
+            .new_string("image/png")
+            .unwrap_or_else(|e| panic!("{e}"))
+            .into()],
+    )
+    .unwrap_or_else(|e| panic!("{e}"));
+
+    // FIXME: Cannot get the result as it's not possible to override
+    //        the `Activity.onActivityResult` method in pure Rust.
+    env.call_method(
+        activity_instance,
+        "startActivityForResult",
+        "(Landroid/content/Intent;I)V",
+        &[intent.clone().into(), 100.into()],
+    )
+    .unwrap_or_else(|e| panic!("{e}"));
 }
